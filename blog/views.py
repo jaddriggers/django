2aea9cd28ae40769950697146c65943cf9f96e59
Jad <Jad@DESKTOP-529D60H>
2021-05-16 17:02:23 -0400

    added user registration page with form. Styled with crispy forms

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,14 @@
 from django.shortcuts import render
+from .models import Post
 
-post = [
-    {
-        'author': 'Jad',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'March 30, 1977'
-    },
-    {
-        'author': 'Keira',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'June 05, 2011'
-    }
-]
 
 def home(request):
     context = {
-        'posts': post
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
+
 def about(request):
-    return render(request, 'blog/about.html', {'title': 'About' })
+    return render(request, 'blog/about.html', {'title': 'About'})
 
